c3914de88da09c90e0385f136ee8db7ce0552c7a
Vivek Vardhan <anapalavivekvardhan5654@gmail.com>
2025-05-26 20:07:51 +0530

    prepared for the developemnt

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express from 'express';  
 import  notesRoutes  from './routes/notesRoutes.js'
 import dotenv from 'dotenv';
+import path from 'path';
 
 
 import { connectMongoose } from './db/connect_db.js';
@@ -10,17 +11,26 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV !== 'production') {
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:5173'
 }));
+}
 
 app.use('/api/notes/',notesRoutes);
-app.listen(5001, () => {
+
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+
+if(process.env.NODE_ENV === 'production') {
+    app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
+}
+app.listen(port, () => {
     console.log(`listening on port ${port}`);
     connectMongoose();
-
-    
 });
-
- 
